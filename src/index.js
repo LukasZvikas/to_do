e7933e6f10cb3a6da6bc2c750b2130c1dccd0a17
LukasZvikas <lzvikas1@gmail.com>
2019-06-28 00:38:11 +0100

    subscribed localStorage getter and setter with redux to persist recorded actions

--- src/index.js
+++ src/index.js
@@ -4,8 +4,17 @@ import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
 import App from './App';
 import appReducer from './reducers';
+import { getState, saveState } from './utilities/localStorage';
 
-const store = createStore(appReducer, {});
+const savedState = getState();
+
+const store = createStore(appReducer, savedState);
+
+store.subscribe(() => {
+  saveState({
+    todos: store.getState().toDos.recordedTasks
+  });
+});
 
 ReactDOM.render(
   <Provider store={store}>
