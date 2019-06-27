ccb656025e7a81d384f3a88d3eaf709abad115c7
LukasZvikas <lzvikas1@gmail.com>
2019-06-27 17:03:20 +0100

    added redux boilerplate

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
 import App from './App';
+import appReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(appReducer, {});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
